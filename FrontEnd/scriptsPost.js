0ec9c20657755ea701ad765a6c2aa603e90a19c7
daisha19 <raissasilvadeoliveira191@gmail.com>
2024-05-01 14:23:04 -0300

    pagPostv0.0

---
+++ FrontEnd/scriptsPost.js
@@ -0,0 +1,8 @@
+/*--SIDEBAR--*/
+    let btn = document.querySelector('#btn');
+    let slidebar = document.querySelector('.sidebar');
+
+    btn.onclick = function() {
+        slidebar.classList.toggle('active');
+    };
+    /*--FIM  SIDEBAR--*/   
