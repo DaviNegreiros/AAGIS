c5f9e4b6d6e8f0752ff4fa755e7bd3e645394891
Luiz Felipe Bessa <bessalfs@gmail.com>
2024-05-10 16:00:50 -0300

    adicionando a base do projeto com css e handlebars

---
+++ BackEnd/projeto oficial aagis/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express() //npm i express
+const handlebars = require('express-handlebars') //npm i express-handlebars
+const bodyParser = require('body-parser') //npm i body-parser
+const Post = require('./models/Post')
+const path = require('path') //npm i path
+const fileUpload = require('express-fileupload') //npm i express-fileupload
+const fs = require('fs')
+//npm install mysql2
+
+//default option
+app.use(fileUpload())
+//app.use(express.static('views'))
+
+//static files
+app.use(express.static('upload'))
+app.use(express.static('public'))
+
+//Config
+//Template Engine
+app.engine('handlebars', handlebars.engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+
+        allowProtoMethodsByDefault: true,
+    }
+}))
+app.set('view engine', 'handlebars')
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Rotas
+//rota home
+app.get('/home', function (req, res) {
+
+    // Defina o caminho para o arquivo CSS
+    const cssPath = './views/styles.css'
+
+    Post.findAll({
+        order: [['id', 'DESC']],//ordenando as noticias por id do maior para o menor
+    }).then(function (posts) {
+        // Adicionando 1 ao índice de cada post
+        const postsWithIndex = posts.map((post, index) => {
+            post.indexPlusOne = index + 1;
+            return post;
+        })
+        res.render('index', { posts: posts, style: 'styles.css' })
+    })
+})
+
+
+//rota login
+app.get('/login', function (req, res) {
+    res.render('pag-login', { style: 'styleLogin.css' })
+})
+
+
+//rota post
+app.get('/post', function (req, res) {
+    res.render('pag-post', { style: 'style-post.css' })
+})
+
+app.listen(7071, function () {
+    console.log("Server on: http://localhost:7071")
+})
---
+++ BackEnd/projeto oficial aagis/models/db.js
@@ -0,0 +1,12 @@
+// Arquivo apenas para se conectar ao banco de dados
+//Conexão com o BD MySQL
+//Model definition
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('aagis3', 'root', '2912', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
